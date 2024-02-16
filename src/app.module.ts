05222d09f5a9b2a74d7ad022f6607613c8c05b4a
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-02-15 21:26:56 -0300

    feat: Add new files and controllers to JWT strategy and sessions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthenticateController } from 'src/controllers/authenticate.controller';
 import { CreateAccountController } from 'src/controllers/create-account.controller';
+import { CreateQuestionController } from 'src/controllers/create-question.controller';
+import { envSchema } from 'src/env';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
