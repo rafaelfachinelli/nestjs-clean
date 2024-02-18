6b52325b005ab38f37bf2362e94accb33a9386ec
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-02-18 08:09:53 -0300

    feat: Add cryptography and repository classes

---
+++ src/domain/forum/application/use-cases/errors/wrong-credentials-error.ts
@@ -0,0 +1,8 @@
+import { UseCaseError } from '@/core/errors/use-case-error';
+
+export class WrongCredentialsError extends Error implements UseCaseError {
+  constructor() {
+    super('Wrong credentials');
+    this.name = 'WrongCredentialsError';
+  }
+}
