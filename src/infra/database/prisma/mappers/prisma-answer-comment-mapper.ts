9b5117c9c4e8a0fb3a265f53f07758b56889270c
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-02-18 21:53:18 -0300

    feat: UpdatedAt field to accept null values in Answer and Comment entities and mappers

---
+++ src/infra/database/prisma/mappers/prisma-answer-comment-mapper.ts
@@ -0,0 +1,36 @@
+import { Comment as PrismaComment, Prisma } from '@prisma/client';
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
+
+export class PrismaAnswerCommentMapper {
+  static toDomain(raw: PrismaComment): AnswerComment {
+    if (!raw.answerId) {
+      throw new Error('AnswerId cannot be null');
+    }
+
+    return AnswerComment.create(
+      {
+        content: raw.content,
+        answerId: new UniqueEntityID(raw.answerId),
+        authorId: new UniqueEntityID(raw.authorId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    );
+  }
+
+  static toPrisma(
+    answerComment: AnswerComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: answerComment.id.toString(),
+      authorId: answerComment.authorId.toString(),
+      answerId: answerComment.answerId.toString(),
+      content: answerComment.content,
+      createdAt: answerComment.createdAt,
+      updatedAt: answerComment.updatedAt,
+    };
+  }
+}
