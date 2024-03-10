a323ae577debafa1e3c808ca64e9f6f302f48d22
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-03-10 17:41:57 -0300

    feat: Create upload attachment controller + add multer type declaration

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -38,6 +38,7 @@ import { FetchQuestionCommentsController } from '@/infra/http/controllers/fetch-
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments';
 import { FetchAnswerCommentsController } from '@/infra/http/controllers/fetch-answer-comments.controller';
 import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments';
+import { UploadAttachmentController } from '@/infra/http/controllers/upload-attachment.controller';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -60,6 +61,7 @@ import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases
     DeleteAnswerCommentController,
     FetchQuestionCommentsController,
     FetchAnswerCommentsController,
+    UploadAttachmentController,
   ],
   providers: [
     CreateQuestionUseCase,
