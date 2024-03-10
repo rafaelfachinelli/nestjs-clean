6d4f53e924edaa8acfd2b66e9e6f2703c485318a
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-03-10 17:14:43 -0300

    feat: Add FetchAnswerCommentsController and FetchAnswerCommentsUseCase

--- src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -1,6 +1,7 @@
 import { Either, right } from '@/core/either';
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
+import { Injectable } from '@nestjs/common';
 
 interface FetchAnswerCommentsUseCaseRequest {
   answerId: string;
@@ -12,6 +13,7 @@ type FetchAnswerCommentsUseCaseResponse = Either<
   { answerComments: AnswerComment[] }
 >;
 
+@Injectable()
 export class FetchAnswerCommentsUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
